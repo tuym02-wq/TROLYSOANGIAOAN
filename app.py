69946138d78296b9753a960309d2f2e4084c7088
tuym02-wq <tuym02@gmail.com>
2025-12-10 11:00:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ st.markdown("""
 st.markdown("""
 <div class="main-header">
     <h1>📘 TRỢ LÝ SOẠN GIÁO ÁN TỰ ĐỘNG (NLS)</h1>
-    <p>Tác giả:Trường PTDTBT Tiểu hoc Ngam La</p>
+    <p>Tác giả: Nguyễn Văn Tuyên Trường PTDTBT Tiểu hoc Ngam La</p>
 </div>
 """, unsafe_allow_html=True)
 
@@ -347,4 +347,4 @@ if st.button("🚀 SOẠN GIÁO ÁN NGAY"):
 
 # --- CHÂN TRANG ---
 st.markdown("---")
-st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Lù Seo Sần - Trường PTDTBT Tiểu học Ngam La</div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Nguyễn Văn Tuyên - Trường PTDTBT Tiểu học Ngam La</div>", unsafe_allow_html=True)
